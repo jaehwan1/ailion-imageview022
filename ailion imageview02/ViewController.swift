dd46617a8194db4136c227174826e31f7b2e6b60
D7703_18 <d7703_18@d7703-18ui-imac.local>
2019-03-28 14:21:23 +0900

    Initial Commit

---
+++ ailion imageview02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ailion imageview02
+//
+//  Created by D7703_18 on 2019. 3. 28..
+//  Copyright © 2019년 A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
