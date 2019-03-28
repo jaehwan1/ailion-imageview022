64cf1dde499267f952c65980a8eba146766fa660
D7703_18 <d7703_18@d7703-18ui-imac.local>
2019-03-28 15:32:07 +0900

    dddd

--- ailion imageview02/ViewController.swift
+++ ailion imageview02/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countlabel: UILabel!
+    
+    var count = 1
+    
+    @IBOutlet weak var ailion: UIImageView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ailion.image = UIImage(named: "frame1.png" )
+        countlabel.text = "frame\(count).png"
     }
+    @IBAction func button(_ sender: Any) {
+       count+=1
+        
+        if(count>5){
+            count = 1
+        }
+        ailion.image = UIImage(named: "frame\(count).png" )
+        countlabel.text = "frame\(count).png"    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
